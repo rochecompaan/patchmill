9950762c4bbd3af5a7b92d295a26c6ae6bca13cc
Roché Compaan <roche@upfrontsoftware.co.za>
2026-05-27 11:28:29 +0200

    feat(nix): add patchmill package derivation

---
+++ nix/package.nix
@@ -0,0 +1,57 @@
+{
+  lib,
+  buildNpmPackage,
+  nodejs_24,
+  makeWrapper,
+}:
+
+let
+  buildNpmPackageNode24 = buildNpmPackage.override { nodejs = nodejs_24; };
+in
+buildNpmPackageNode24 rec {
+  pname = "patchmill";
+  version = "0.0.0";
+
+  src = lib.cleanSourceWith {
+    src = lib.cleanSource ../.;
+    filter = path: type:
+      let
+        baseName = baseNameOf path;
+      in
+      !(baseName == ".git"
+        || baseName == "node_modules"
+        || baseName == ".devenv"
+        || baseName == ".patchmill"
+        || baseName == "result");
+  };
+
+  npmDepsHash = lib.fakeHash;
+
+  dontNpmBuild = true;
+
+  nativeBuildInputs = [ makeWrapper ];
+
+  env = {
+    HUSKY = "0";
+  };
+
+  doCheck = true;
+  checkPhase = ''
+    runHook preCheck
+    npm test
+    runHook postCheck
+  '';
+
+  postInstall = ''
+    rm -f "$out/bin/patchmill"
+    makeWrapper ${nodejs_24}/bin/node "$out/bin/patchmill" \
+      --add-flags "$out/lib/node_modules/${pname}/bin/patchmill.ts"
+  '';
+
+  meta = {
+    description = "Agent-driven software factory that turns issues into reviewed diffs";
+    mainProgram = "patchmill";
+    license = lib.licenses.mit;
+    platforms = lib.platforms.unix;
+  };
+}
