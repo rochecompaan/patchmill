006fe2929e5b479159a5234f35754ab5a97aee3a
rochecompaan <784273+rochecompaan@users.noreply.github.com>
2026-08-21 07:27:39 +0000

    chore(deps): update pi-subagents to 0.53.0

--- nix/package.nix
+++ nix/package.nix
@@ -26,7 +26,7 @@ buildNpmPackageNode24 rec {
         || baseName == "result");
   };
 
-  npmDepsHash = "sha256-UhW5Db+3qjMwloeGt8OrwgpEVQ6GTCFi5EWhLMwlVu4=";
+  npmDepsHash = "sha256-/KKcoibYe0dXS074nGtmfEOcDKsc1wpGUuqLe8TQlv8=";
   npmDepsFetcherVersion = 2;
 
   dontNpmBuild = true;
